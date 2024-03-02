e12e330c9a1da87f0276367d0590997874ff426a
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2024-03-02 14:38:07 +0330

    [ADD] transfer test

--- tests/Feature/TransferTest.php
+++ tests/Feature/TransferTest.php
@@ -2,17 +2,28 @@
 
 namespace Tests\Feature;
 
+use App\Models\Account;
+use App\Models\Transaction;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TransferTest extends TestCase
 {
-    public function test_source_is_valid(): void
+    public function test_both_valid(): void
     {
+        $srcAccount = Account::find(1);
+        $destAccount = Account::find(2);
+        $srcBallance = $srcAccount->ballance;
+        $destBallance = $destAccount->ballance;
         $response = $this->json('POST', '/api/transfer', [
-            'amount' => 1000, 'source' => '6274129005473742', 'destination' => '6104337465312385'
+            'amount' => 2121, 'source' => '6274129005473742', 'destination' => '6104337465312385'
         ]);
         $response->assertStatus(200);
+        $this->assertDatabaseHas(Transaction::class, ['source_card_id' => 1, 'dest_card_id' => 3, 'amount' => 2121]);
+        $srcAccount->refresh();
+        $destAccount->refresh();
+        $this->assertEquals($srcBallance - 2121 - 500, $srcAccount->ballance);
+        $this->assertEquals($destBallance + 2121, $destAccount->ballance);
     }
 }
